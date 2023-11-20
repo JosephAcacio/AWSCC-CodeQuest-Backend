cf62dd888d493ebf6d529982b7a5b445e2b31fdb
Joseph Acacio <josephcacacio@iskolarngbayan.pup.edu.ph>
2023-11-20 23:22:36 +0800

    Update day1challenge.py

--- submissions/challenges/day1challenge.py
+++ submissions/challenges/day1challenge.py
@@ -4,3 +4,5 @@ isMale = True
 department = "Department of Software and Web Development"
 
 print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
+
+#Joseph C. Acacio is an 18-year old male participating in Department of Software and Web Development's 30 Days of Coding.
