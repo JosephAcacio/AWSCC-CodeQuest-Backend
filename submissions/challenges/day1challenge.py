fba3efd1fe94d9f1ed3a92ef9fc0672674ee1ab2
Joseph Acacio <josephcacacio@iskolarngbayan.pup.edu.ph>
2023-11-20 23:15:50 +0800

    Create day1challenge.py

---
+++ submissions/challenges/day1challenge.py
@@ -0,0 +1,6 @@
+name = "Joseph C. Acacio"
+age = 18
+isMale = True
+department = "Department of Software and Web Development"
+
+print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
