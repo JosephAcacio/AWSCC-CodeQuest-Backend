5f673121491e6f1f6ae045a2cbd251735026c233
Joseph Acacio <josephcacacio@iskolarngbayan.pup.edu.ph>
2023-11-21 20:59:00 +0800

    Update day2 Challenge

---
+++ submissions/challenges/day2challenge.py
@@ -0,0 +1,7 @@
+first_num = input("Enter first number: ");
+second_num = input("Enter second number: ");
+third_num = input("Enter third number: ");
+
+sum = int(first_num) + int(second_num) + int(third_num);
+
+print(f"Sum: {sum}");
