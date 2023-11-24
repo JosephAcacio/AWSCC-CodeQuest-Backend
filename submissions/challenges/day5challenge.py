357f076ab43baad6c24020c7e8b2dd76f4f1bcd7
Joseph Acacio <josephcacacio@iskolarngbayan.pup.edu.ph>
2023-11-24 22:36:03 +0800

    finished day5challenge

---
+++ submissions/challenges/day5challenge.py
@@ -0,0 +1,6 @@
+import random;
+
+myCourses = ["MMW", "PathFit", "Comprog", "ITC", "NSTP", "Purcomm", "PolGov", "FPK"];
+computer_chosenCourse = random.choice(myCourses);
+
+print(f"{computer_chosenCourse} is my favorite subject");
