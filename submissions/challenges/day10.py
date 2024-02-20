2f3544bb21a007f9b0ea76e900a2ca0a4d09b594
Joseph Acacio <josephcacacio@iskolarngbayan.pup.edu.ph>
2024-02-20 19:59:36 +0800

    Update day10.py

--- submissions/challenges/day10.py
+++ submissions/challenges/day10.py
@@ -3,7 +3,7 @@ from module import divide_numbers
 try:
     a = int(input("Enter the dividend: "));
     b = int(input("Enter the divisor: "));
-    quotient = divide_numbers(a,b)
+    quotient = "{:.2f}".format(divide_numbers(a,b));
 except Exception as e:
     print(f"Invalid input: {e}")
 else:
